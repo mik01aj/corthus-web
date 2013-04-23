ba45923b445dc95f9d708f1cf543679c1cc3d71f
Mikołaj Dądela <mik01aj@o2.pl>
2013-04-23 08:23:21 +0200

    Corrected starting bytes of some texts.

--- corthus-web/scripts/remove_start_chars.py
+++ corthus-web/scripts/remove_start_chars.py
@@ -9,6 +9,33 @@ for fn in os.listdir('.'):
     if s.startswith('\xef\xbb\xbf'):
         open(fn, 'w').write(s[3:])
 
+
+
+
+
+for d, ds, fs in os.walk('corthus-web'):
+    for f in fs:
+        if f.endswith('.text'):
+            s = open(d + '/' + f).read()
+            assert s
+            if s.startswith('\xef\xbb\xbf'):
+                open(f, 'w').write(s[3:])
+
+
+
+for d, ds, fs in os.walk('corthus-web'):
+    for f in fs:
+        if f.endswith('.text'):
+            s = open(d + '/' + f).read()
+            assert s
+            if not s.startswith('#'):
+                print d + '/' + f
+                print s.splitlines()[0]
+
+
+
+
+
 """
 
 print __doc__
